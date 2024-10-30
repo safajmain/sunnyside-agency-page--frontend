6574931324db0d9dc9ae7c64abf7564ad9108822
safajmain <me23mo44@gmail.com>
2024-10-30 20:20:06 +0300

    add html+css stop footer

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-const mobileNavButon = document.querySelector(".mobile-menu");
-const mobileNav = document.querySelector(".mobile-nav");
-
-function displayMobileNav() {
-  if (mobileNav.classList.contains("active")) {
-    mobileNav.classList.remove("active");
-    mobileNavButon.style.opacity = 1;
-  } else {
-    mobileNav.classList.add("active");
-    mobileNavButon.style.opacity = 0.5;
-  }
-}
-
-mobileNavButon.addEventListener("click", displayMobileNav);
+ 
